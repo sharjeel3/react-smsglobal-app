6e2f472ba637098faa6f2cd34bece18ca8b84fe7
Sharjeel <ahmadsharjeel3@gmail.com>
2019-11-11 20:48:54 +1100

    Structure layout and router

--- src/app/Router.js
+++ src/app/Router.js
@@ -1,5 +1,6 @@
 import React, { Suspense } from 'react';
 import { BrowserRouter as ReactRouter, Route, Switch } from 'react-router-dom';
+import { Layout } from './Layout';
 
 const SettingView = React.lazy(() => import('../views/Setting'));
 const SendMessageView = React.lazy(() => import('../views/SendMessage'));
@@ -9,22 +10,24 @@ const HomeView = React.lazy(() => import('../views/Home'));
 export const Router = () => {
   return (
     <ReactRouter>
-      <Suspense fallback={<div>Loading...</div>}>
-        <Switch>
-          <Route path="/setting">
-            <SettingView />
-          </Route>
-          <Route path="/send">
-            <SendMessageView />
-          </Route>
-          <Route path="/report">
-            <ReportView />
-          </Route>
-          <Route path="/">
-            <HomeView />
-          </Route>
-        </Switch>
-      </Suspense>
+      <Layout>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Switch>
+            <Route path="/setting">
+              <SettingView />
+            </Route>
+            <Route path="/send">
+              <SendMessageView />
+            </Route>
+            <Route path="/report">
+              <ReportView />
+            </Route>
+            <Route path="/">
+              <HomeView />
+            </Route>
+          </Switch>
+        </Suspense>
+      </Layout>
     </ReactRouter>
   );
 };
--- src/app/index.js
+++ src/app/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { createGlobalStyle } from 'styled-components';
 import { globleStyles } from '../ui-library/global';
-import { Layout } from './Layout';
 import { Router } from './Router';
 
 const GlobalStyles = createGlobalStyle`
@@ -12,9 +11,7 @@ export const App = () => {
   return (
     <>
       <GlobalStyles />
-      <Layout>
-        <Router />
-      </Layout>
+      <Router />
     </>
   );
 };
