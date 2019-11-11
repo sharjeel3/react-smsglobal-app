eea76f2f3bc57874f0702af9dca34288f2f68a2e
Sharjeel <ahmadsharjeel3@gmail.com>
2019-11-12 00:50:17 +1100

    Add routes mapping for navigation

---
+++ src/constants/routesMap.js
@@ -0,0 +1,6 @@
+export const navigationRoutes = [
+  { title: 'Home', path: '/' },
+  { title: 'Settings', path: '/setting' },
+  { title: 'Send Message', path: '/send' },
+  { title: 'Reports', path: '/report' }
+];
