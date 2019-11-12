1635b0c23c2589d501af83d6c7571c2b15aa1b6f
Sharjeel <ahmadsharjeel3@gmail.com>
2019-11-12 11:12:29 +1100

    Refactor api data into helper

--- src/redux/actions/app/index.js
+++ src/redux/actions/app/index.js
@@ -6,11 +6,16 @@ import {
 } from '../../../constants/formFields';
 import { LocalStorage } from '../../../lib/LocalStorage';
 
-export const loadApiConfig = () => {
+export const getApiData = () => {
   const apiKey = LocalStorage.get(API_KEY_FIELD) || '';
   const apiSecret = LocalStorage.get(API_SECRET_FIELD) || '';
   const apiDisplayName = LocalStorage.get(API_DISPLAY_NAME_FIELD) || '';
 
+  return { apiKey, apiSecret, apiDisplayName };
+}
+
+export const loadApiConfig = () => {
+  const { apiKey, apiSecret, apiDisplayName } = getApiData();
   return {
     type: LOAD_API_CONFIG,
     payload: {
