fad0be610cb832cc484c1647edbd50fdd7a3c586
Sharjeel <ahmadsharjeel3@gmail.com>
2019-11-12 16:14:10 +1100

    Fix authorization header generation

--- src/lib/Axios/index.js
+++ src/lib/Axios/index.js
@@ -5,33 +5,24 @@ import Crypto from 'crypto-js';
 export const Axios = (() => {
   const host = 'api.smsglobal.com';
 
-  const getAuthorizationHeader = uri => {
+  const getAuthorizationHeader = (uri, method) => {
     const { apiKey, apiSecret } = getApiData();
 
-    const ts = new Date().getTime();
-    const nonce = Math.floor(Math.random() * 10000000);
+    const ts = Math.floor(new Date().getTime() / 1000);
+    const nonce = Math.floor(Math.random() * 1e16);
 
-    const macString = `${ts}\n
-     ${nonce}\n
-     POST\n
-     ${uri}\n
-     api.smsglobal.com\n
-     80\n
-     \n `;
-
+    const signature = [ts, nonce, method, uri, host, 80];
+    const macString = `${signature.join(`\n`)}\n\n`;
     const macHash = Crypto.HmacSHA256(macString, apiSecret);
+    const macBase64 = Crypto.enc.Base64.stringify(macHash);
 
-    const authorizationHeader = `MAC id="${apiKey}", ts="${ts}", nonce="${nonce}", mac="${macHash.toString(
-      Crypto.enc.Base64
-    )}"`;
-
-    return authorizationHeader;
+    return `MAC id="${apiKey}", ts="${ts}", nonce="${nonce}", mac="${macBase64}"`;
   };
 
   const fetchApi = props => {
-    const { method = 'get', uri, data = {}, headers = {} } = props;
+    const { method = 'GET', uri, data = {}, headers = {} } = props;
 
-    const authorizationHeader = getAuthorizationHeader(uri);
+    const authorizationHeader = getAuthorizationHeader(uri, method);
 
     return axios({
       url: `http://${host}${uri}`,
