1c42a68f47f3febe751c9f7e2673fd9480883949
Sharjeel <ahmadsharjeel3@gmail.com>
2019-11-13 10:12:26 +1100

    Add test for app reducer

---
+++ src/redux/reducers/app/app.test.js
@@ -0,0 +1,20 @@
+import { appReducer } from './index';
+import { LOAD_API_CONFIG } from '../../../constants/actionTypes';
+
+describe('REDUCER - APP', () => {
+  const action = {
+    type: LOAD_API_CONFIG,
+    payload: {
+      apiKey: 'test',
+      apiSecret: 'testttest',
+      name: 'name',
+      isConfigReady: true,
+      hasValidSettings: true
+    }
+  };
+  it('should update api config', () => {
+    expect(appReducer(undefined, action)).toEqual({
+      api: { ...action.payload }
+    });
+  });
+});
