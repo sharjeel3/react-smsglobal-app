a679f0e8c047fa8da508068401f9b26ea14ea224
Sharjeel <ahmadsharjeel3@gmail.com>
2019-11-13 12:52:39 +1100

    Update saga handler

--- src/redux/actions/message/index.js
+++ src/redux/actions/message/index.js
@@ -1,7 +1,7 @@
 import {
   SEND_MESSAGE_REQUESTED,
   GET_MESSAGE_REQUESTED,
-  REFRESH_STATE_ITEM
+  REFRESH_STATE_ITEM, GET_MESSAGE_SUCCEEDED, GET_MESSAGE_FAILED
 } from '../../../constants/actionTypes';
 
 export const sendMessage = (origin, destination, message) => ({
@@ -17,4 +17,16 @@ export const getMessage = () => ({
 export const refreshStateItem = payload => ({
   type: REFRESH_STATE_ITEM,
   payload
+});
+
+export const refreshGetMessageSuccess = (error, response) => ({
+  type: GET_MESSAGE_SUCCEEDED,
+  error,
+  response
+});
+
+export const refreshGetMessageFailure = (error, response) => ({
+  type: GET_MESSAGE_FAILED,
+  error,
+  response
 });
--- src/redux/sagas/message/index.js
+++ src/redux/sagas/message/index.js
@@ -3,11 +3,10 @@ import {
   SEND_MESSAGE_REQUESTED,
   SEND_MESSAGE_SUCCEEDED,
   SEND_MESSAGE_FAILED,
-  GET_MESSAGE_FAILED,
-  GET_MESSAGE_REQUESTED,
-  GET_MESSAGE_SUCCEEDED
+  GET_MESSAGE_REQUESTED
 } from '../../../constants/actionTypes';
 import { Api } from '../../../lib/Api';
+import { refreshGetMessageFailure, refreshGetMessageSuccess } from '../../actions/message';
 
 export function* getMessageWorker(api) {
   function* handler(action) {
@@ -15,12 +14,12 @@ export function* getMessageWorker(api) {
       const [error, response] = yield call(api.getMessage, action.payload);
       if (error) {
         // Most likely Api error
-        return yield put({ type: GET_MESSAGE_FAILED, error: error.response.data, response: null });
+        return yield put(refreshGetMessageFailure(error.response.data, null));
       }
-      yield put({ type: GET_MESSAGE_SUCCEEDED, error: null, response });
+      yield put(refreshGetMessageSuccess(null, response));
     } catch (e) {
       // Something broke down
-      yield put({ type: GET_MESSAGE_FAILED, error: e.toString(), response: null });
+      yield put(refreshGetMessageFailure(e.toString(), null));
     }
   }
   yield takeEvery(GET_MESSAGE_REQUESTED, handler);
@@ -28,6 +27,7 @@ export function* getMessageWorker(api) {
 
 export function* sendMessageWorker(api) {
   function* handler(action) {
+    // TODO: use action creators to get actions
     try {
       const [error, response] = yield call(api.sendMessage, action.payload);
       if (error) {
